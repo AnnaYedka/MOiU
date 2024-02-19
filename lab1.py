db9fb51cae094c4281496032987c3aaed6830ff6
annayedka <anna.yedka@gmail.com>
2024-02-19 15:57:53 +0300

    lab1 with output

--- lab1.py
+++ lab1.py
@@ -1,34 +1,37 @@
 import numpy as np
 
-n = 3
-i = 2
+def inverse_matrix(n, i):
+	if not n or i >=n:
+		raise Exception('invalid arguments')
 
-A = np.random.randint(0, 20, size=(n, n))
-x = np.random.randint(0, 20, size=(n,))
+	A = np.random.randint(0, 20, size=(n, n))
+	x = np.random.randint(0, 20, size=(n,))
 
-inv_A = np.linalg.inv(A)
-l = inv_A @ x
+	print("A = ", A)
+	print("x = ", x)
 
-if not l[i]:
-	print("replaced matrix is not inversable")
-	raise Exception
+	inv_A = np.linalg.inv(A)
+	l = inv_A @ x
 
-l_wave = l.copy()
-l_wave[i] = -1
-l_top = l_wave / -l[i]
-Q = np.eye(n)
-Q[:, i] = l_top
+	if not l[i]:
+		print("replaced matrix is not inversable")
+		return
 
+	l_wave = l.copy()
+	l_wave[i] = -1
+	l_top = l_wave / -l[i]
+	Q = np.eye(n)
+	Q[:, i] = l_top
 
-res_inv = np.empty((n, n))
+	res_inv = np.empty((n, n))
 
-for j in range(n):
-	for k in range(n):
-		if j == i:
-			res_inv[j][k] = Q[j][j] * inv_A[j][k]
-		else:
-			res_inv[j][k] = Q[j][i] * inv_A[i][k] + Q[j][j] * inv_A[j][k]
+	for j in range(n):
+		for k in range(n):
+			if j == i:
+				res_inv[j][k] = Q[j][j] * inv_A[j][k]
+			else:
+				res_inv[j][k] = Q[j][i] * inv_A[i][k] + Q[j][j] * inv_A[j][k]
 
-print(Q @ inv_A)
+	print("inversed matrix O(n3): ", Q @ inv_A)
 
-print(res_inv)
+	print("inversed matrix O(n2): ", res_inv)
