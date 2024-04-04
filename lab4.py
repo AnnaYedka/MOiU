8248bbd6978fb57956c0ed121f3742c65f1fc6ef
annayedka <anna.yedka@gmail.com>
2024-04-04 16:34:52 +0300

    lab 4

---
+++ lab4.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+import lab1
+
+
+def dual_simplex_method(c, A, b, B):
+    while True:
+        # step 1
+        AB_inv = np.linalg.inv(A[:, B])
+
+        # step 2
+        cB = c[B]
+
+        # step 3
+        y = cB @ AB_inv
+
+        # step 4
+        n = len(A[0])
+        kB = AB_inv @ b
+
+        k = np.zeros(n)
+        for i in range(len(B)):
+            k[B[i]] = kB[i]
+
+        # step 5
+        if np.all(k >= 0):
+            return k
+
+        # step 6
+
+        jk = np.where(k < 0)[0][0]
+        k_indx = np.where(B == jk)[0][0]
+
+        # step 7
+        delta_y = AB_inv[k_indx]
+        mu = np.zeros(n)
+        indexes = np.setdiff1d(np.arange(n), B)
+        for j in indexes:
+            mu[j] = delta_y @ A[:, j]
+
+        # step 8
+        if np.all(mu >= 0):
+            raise Exception('задача несовместна')
+
+        sigma = np.full(n, np.inf)
+        for j in indexes:
+            if mu[j] < 0:
+                sigma[j] = (c[j] - A[:, j] @ y) / mu[j]
+
+        j_0 = np.argmin(sigma)
+        B[k_indx] = j_0
+
+
+if __name__ == "__main__":
+    c = np.array([-4, -3, -7, 0, 0])
+    A = np.array([
+        [-2, -1, -4, 1, 0],
+        [-2, -2, -2, 0, 1],
+    ])
+    b = np.array([-1, -1.5])
+    B = np.array([3, 4])
+
+    x = dual_simplex_method(c, A, b, B)
+    print(x)
